5375b71c0a2e1455ef401e039ad0832ffcd479dc
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-20 10:44:57 -0400

    updating controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  resources :products
+  # get 'products/index'
+  #
+  # get 'products/show'
+  #
+  # get 'products/new'
+  #
+  # get 'products/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
 end
